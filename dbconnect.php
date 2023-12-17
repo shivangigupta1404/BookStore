4a3e9f4faadb0c3ef1a52bbbbe00297f960269aa
adzizPe <adzanaziz2@gmail.com>
2023-12-18 00:17:12 +0700

    modified

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php 
-define('DB_HOST', 'host here'); 
-define('DB_NAME', 'dbname here'); 
-define('DB_USER','username here'); 
-define('DB_PASSWORD','password here'); 
+define('DB_HOST', 'localhost'); 
+define('DB_NAME', 'bookstore'); 
+define('DB_USER','root'); 
+define('DB_PASSWORD',''); 
 
 $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
 $db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysql_error()); 
